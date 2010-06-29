4ef00d8b8fdca3737106f827bdc068bc95946fe0
saxjazman9 <saxjazman9@gmail.com>
2010-06-29 22:19:56 +0000

    adding apikeys.sql to rebuild_database.sh script

--- trunk/rebuild_database.sh
+++ trunk/rebuild_database.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DB="cif"
-SCHEMA="messages.sql domain.sql inet.sql malware.sql url.sql email.sql routeviews.sql"
+SCHEMA="messages.sql domain.sql inet.sql malware.sql url.sql email.sql routeviews.sql apikeys.sql"
 
 psql -U postgres -c "DROP DATABASE $DB"
 psql -U postgres -c "CREATE DATABASE $DB"
