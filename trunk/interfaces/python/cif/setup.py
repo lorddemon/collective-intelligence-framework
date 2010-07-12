4b19db1d8addf9e3b81d9739815737ac124e33a9
saxjazman9@gmail.com <saxjazman9@gmail.com@f9ffa795-1512-f4ca-611e-4b753ca244e6>
2010-07-12 21:45:46 +0000

    version bump, fixed setup.py install_requires, commited to cheeseshop _02

--- trunk/interfaces/python/cif/setup.py
+++ trunk/interfaces/python/cif/setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='cif',
-    version='0.00_01',
+    version='0.00_02',
     description="",
     author="Wes Young",
     packages=find_packages(),
     zip_safe = False,
+    install_requires= ['restclient','simplejson','Texttable'],
 )
