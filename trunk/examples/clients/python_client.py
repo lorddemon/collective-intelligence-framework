fdcc59820274d8ee6587a8ca4dcdcb838f2ecaa4
saxjazman9@gmail.com <saxjazman9@gmail.com@f9ffa795-1512-f4ca-611e-4b753ca244e6>
2010-11-08 15:11:18 +0000

    cleaning up perl/python client code

--- trunk/examples/clients/python_client.py
+++ trunk/examples/clients/python_client.py
@@ -10,22 +10,20 @@ def usage():
     print """Usage: python """ + sys.argv[0] + """ -q xyz.com -f table
         -h  --help:                 this message
         -d  --debug:                debug output
-        -f  --format [raw|table]:   output format
         -q <string>:                query string
 
     configuration file ~/.cif should be readable and look something like:
     
     [client]
-    url=https://example.com:443/REST/1.0/cif
+    url=https://example.com:443/api
     apikey=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
 
 """
 
 def main(argv):
-    format = 'json'
     query = ''
     try:
-        opts, args = getopt.getopt(argv,"dhf:q:",["debug","help","format=","query="])
+        opts, args = getopt.getopt(argv,"dhf:q:",["debug","help","query="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -36,12 +34,6 @@ def main(argv):
             sys.exit()
         elif opt == '-f':
             format = arg
-            if format not in ("raw","table"):
-                usage()
-                sys.exit()
-            format = 'json'
-            if arg == 'raw':
-                format = 'text'
         elif opt == '-q':
             query = arg
         elif opt == '-d':
@@ -51,25 +43,13 @@ def main(argv):
         usage()
         sys.exit()
 
-    do_search(query, format)
+    do_search(query)
 
-def do_search(query, format):
+def do_search(query):
     rclient = ClientINI()
-    rclient.format = format
     r = rclient.search(query)
-    array = r.split('\n')
-        
-    #this stuff belongs in the client
-    p_status = re.compile('^RT.* 200 Ok (\d+)\/\d+.*$')
-    m = p_status.match(array[0])
-
-    if (int(m.group(1)) > 0):
-        if format == 'text':
-            for a in array:
-                print a
-        else:
-            print rclient.table(array[2])
-
+    
+    print rclient.table(r)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
