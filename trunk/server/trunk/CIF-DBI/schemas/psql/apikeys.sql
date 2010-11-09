ad04783f67007585ced53587d4b4aa087e88dd09
saxjazman9 <saxjazman9@gmail.com>
2010-11-09 19:34:12 +0000

    merged with CIF::WebAPI

--- trunk/server/trunk/CIF-DBI/schemas/psql/apikeys.sql
+++ trunk/server/trunk/CIF-DBI/schemas/psql/apikeys.sql
@@ -1,19 +1,8 @@
 DROP TABLE IF EXISTS apikeys;
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY NOT NULL,
-    email text NOT NULL,
-    firstname text,
-    lastname text,
-    affiliation text,
-    created timestamp with time zone DEFAULT NOW(),
-    UNIQUE(email)
-);
-
 CREATE TABLE apikeys (
     id BIGSERIAL PRIMARY KEY NOT NULL,
+    userid text NOT NULL,
     apikey uuid NOT NULL,
-    userid bigint REFERENCES users(id) ON DELETE CASCADE NOT NULL,
     created timestamp with time zone DEFAULT NOW(),
     UNIQUE(apikey,userid)
 );
