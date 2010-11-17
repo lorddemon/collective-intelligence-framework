08fae8eab9c34743dc12f48df371aaf3f44a1bbc
saxjazman9@gmail.com <saxjazman9@gmail.com@f9ffa795-1512-f4ca-611e-4b753ca244e6>
2010-11-17 15:56:02 +0000

    adding malwarepatrol url and binary feeds

---
+++ trunk/client/trunk/feedsources/malware/mpatrol_urls.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use XML::LibXML 1.70 qw(:threads_shared);
+use Data::Dumper;
+use DateTime;
+use DateTime::Format::DateParse;
+use LWP::Simple;
+use Digest::MD5 qw(md5_hex);
+use Unicode::String qw/utf8/;
+
+use CIF::Message::URLMalware;
+
+my $feed = 'http://www.malware.com.br/cgi/submit?action=list_xml';
+
+my $parser = XML::LibXML->new();
+my $xml = get($feed) || die('failed to get feed: '.$!);
+my $doc = $parser->load_xml(string => $xml);
+
+my $x = 0;
+my @nodes = $doc->findnodes('//url');
+warn 'inserting '.$#nodes.'+ nodes';
+foreach (@nodes){
+    my $node = $_;
+
+    my $id = $node->findvalue('./id');
+    my $url = $node->findvalue('./uri');
+    my $reported = DateTime::Format::DateParse->parse_datetime($node->findvalue('./date'));
+    my $desc = $node->findvalue('./av_info');
+    
+    $url = utf8($url);
+    $url = $url->utf8();
+
+    my $uuid = CIF::Message::URLMalware->insert({
+        address     => $url,
+        impact      => 'malware url',
+        source      => 'malware.com.br',
+        description => 'malware url '.$desc,
+        severity    => 'medium',
+        confidence  => 5,
+        restriction => 'need-to-know',
+        detecttime  => $reported,
+        alternativeid  => 'http://www.malware.com.br/cgi/search.pl?id='.$id,
+        alternativeid_restriction => 'public',
+    });
+    warn $uuid;
+}
