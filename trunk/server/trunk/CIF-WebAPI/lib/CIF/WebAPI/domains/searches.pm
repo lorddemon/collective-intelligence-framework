b16c57b4c33b9eead43fb9a880b656a9db5093ab
saxjazman9 <saxjazman9@gmail.com>
2010-11-23 17:27:13 +0000

    adding code to record searches

---
+++ trunk/server/trunk/CIF-WebAPI/lib/CIF/WebAPI/domains/searches.pm
@@ -0,0 +1,38 @@
+package CIF::WebAPI::domains::searches;
+use base 'CIF::WebAPI';
+
+use strict;
+use warnings;
+
+use CIF::Message::DomainSearch;
+
+sub GET {
+    my ($self, $request, $response) = @_;
+
+    if(exists($self->{'domain'})){
+        my $arg = $self->domain();
+        my @recs = CIF::Message::DomainSearch->search_by_address('%'.$arg.'%',10);
+        if(@recs){
+            my @res = map { CIF::WebAPI::domains::mapIndex($_) } @recs;
+            $response->data()->{'result'} = \@res;
+        }
+        return Apache2::Const::HTTP_OK;
+    } else {
+        my $maxdays = $request->{'r'}->param('age') || $request->dir_config->{'CIFFeedAgeDefault'} || 30;
+        my $maxresults = $request->{'r'}->param('maxresults') || $request->dir_config->{'CIFFeedResultsDefault'} || 10000;
+        my $detecttime = DateTime->from_epoch(epoch => (time() - (84600 * $maxdays)));
+        my @recs = CIF::Message::DomainSearch->search_feed($detecttime,$maxresults);
+        return CIF::WebAPI::domains::generateFeed($response,@recs);
+    }
+
+}
+
+sub buildNext {
+    my ($self,$frag,$req) = @_;
+    
+    my $h = CIF::WebAPI::domains::searches->new($self);
+    $h->{'domain'} = $frag;
+    return $h;
+}
+
+1;
