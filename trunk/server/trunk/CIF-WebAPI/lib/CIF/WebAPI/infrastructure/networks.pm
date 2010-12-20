a281e18252e837a8c03f22abc076ba1296778356
saxjazman9 <saxjazman9@gmail.com>
2010-12-20 21:08:54 +0000

    clean

--- trunk/server/trunk/CIF-WebAPI/lib/CIF/WebAPI/infrastructure/networks.pm
+++ trunk/server/trunk/CIF-WebAPI/lib/CIF/WebAPI/infrastructure/networks.pm
@@ -4,16 +4,4 @@ use base 'CIF::WebAPI';
 use strict;
 use warnings;
 
-use CIF::Message::InfrastructureNetwork;
-
-sub GET {
-    my ($self, $request, $response) = @_;
-    my $maxdays = $request->{'r'}->param('age') || $request->dir_config->{'CIFFeedAgeDefault'} || 30;
-    my $maxresults = $request->{'r'}->param('maxresults') || $request->dir_config->{'CIFFeedResultsDefault'} || 10000;
-
-    my $detecttime = DateTime->from_epoch(epoch => (time() - (84600 * $maxdays)));
-    my @recs = CIF::Message::InfrastructureNetwork->search_feed($detecttime,$maxresults);
-    return CIF::WebAPI::infrastructure::generateFeed($response,@recs);
-}
-
 1;
