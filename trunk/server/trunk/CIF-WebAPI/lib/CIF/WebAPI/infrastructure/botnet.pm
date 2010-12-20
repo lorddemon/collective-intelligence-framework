ae701976fb6b01ea355687455dd3b3fd46b2e964
saxjazman9 <saxjazman9@gmail.com>
2010-12-20 20:51:59 +0000

    dont need these anymore with the new feed component

--- trunk/server/trunk/CIF-WebAPI/lib/CIF/WebAPI/infrastructure/botnet.pm
+++ trunk/server/trunk/CIF-WebAPI/lib/CIF/WebAPI/infrastructure/botnet.pm
@@ -4,10 +4,4 @@ use base 'CIF::WebAPI';
 use strict;
 use warnings;
 
-sub GET {
-    my ($self, $request, $response) = @_;
-    my @recs = $self->SUPER::GET($request,$response);
-    return CIF::WebAPI::infrastructure::generateFeed($response,@recs);
-}
-
 1;
