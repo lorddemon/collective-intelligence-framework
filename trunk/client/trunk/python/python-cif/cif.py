8e489c9ca7de5ebaa66afda8da9d4022bdeec9a3
saxjazman9 <saxjazman9@gmail.com>
2010-12-29 16:20:17 +0000

    updated python client for 0.00_04 release

--- trunk/client/trunk/python/python-cif/cif.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-from cif.client import ClientINI
-
-rclient = ClientINI()
-r = rclient.GET('infrastructure','high')
-#r = rclient.GET('213.109.96.0/22')
-print rclient.table(r)
--- trunk/client/trunk/python/python-cif/setup.py
+++ trunk/client/trunk/python/python-cif/setup.py
@@ -4,8 +4,14 @@ setup(
     name='cif',
     version='0.00_04',
     description="",
-    author="Wes Young",
+    author = 'Wes Young',
+    author_email = 'ci-framework@googlegroups.com',
+    url = 'http://code.google.com/p/collective-intelligence-framework/',
+    license = 'BSD',
     packages=find_packages(),
     zip_safe = False,
-    install_requires= ['restclient','simplejson','Texttable','hashlib','gzip','magic','base64'],
+    install_requires= ['restclient','simplejson','Texttable','python-magic','argparse'],
+    scripts = [
+        'scripts/cifcli.py'
+    ]
 )
