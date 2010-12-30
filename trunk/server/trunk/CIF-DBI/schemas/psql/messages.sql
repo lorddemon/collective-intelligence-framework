72eee82c55403c4efe9068cce9f15efae9eae0ab
saxjazman9@gmail.com <saxjazman9@gmail.com@f9ffa795-1512-f4ca-611e-4b753ca244e6>
2010-12-30 14:51:38 +0000

    saving

--- trunk/server/trunk/CIF-DBI/schemas/psql/messages.sql
+++ trunk/server/trunk/CIF-DBI/schemas/psql/messages.sql
@@ -1,20 +1,21 @@
 DROP VIEW IF EXISTS v_messages;
 DROP TABLE IF EXISTS messages_structured;
 DROP TABLE IF EXISTS messages_unstructured;
+DROP TABLE IF EXISTS messages_blob;
 DROP TABLE IF EXISTS messages;
 
 CREATE TABLE messages (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     uuid uuid NOT NULL,
     source uuid NOT NULL,
-    type VARCHAR(16) CHECK (type IN ('structured','unstructured')) DEFAULT 'unstructured' NOT NULL,
+    type VARCHAR(16) not null,
     format VARCHAR(32), -- IODEF, MetaSharing, IRC, Email, etc...
     confidence REAL,
     severity VARCHAR(6) CHECK (severity IN ('low','medium','high')),
     description text,
     impact VARCHAR(140),
     restriction VARCHAR(16) CHECK (restriction IN ('default','private','need-to-know','public')) DEFAULT 'private' NOT NULL,
-    detecttime timestamp with time zone,
+    detecttime timestamp with time zone DEFAULT NOW(),
     created timestamp with time zone DEFAULT NOW(),
     UNIQUE (uuid)
 );
@@ -33,6 +34,13 @@ CREATE TABLE messages_unstructured (
     source uuid NOT NULL,
     message text NOT NULL,
     UNIQUE(uuid)
+);
+
+CREATE TABLE messages_blob (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    uuid uuid references messages(uuid) on delete cascade not null,
+    message bytea not null,
+    unique(uuid)
 );
 
 CREATE VIEW v_messages AS
