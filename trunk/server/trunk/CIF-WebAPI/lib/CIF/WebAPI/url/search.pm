b1f0c06577d054a339f108db8a0c8fa3af9e9882
saxjazman9 <saxjazman9@gmail.com>
2010-12-31 19:47:38 +0000

    refactor

--- trunk/server/trunk/CIF-WebAPI/lib/CIF/WebAPI/url/search.pm
+++ trunk/server/trunk/CIF-WebAPI/lib/CIF/WebAPI/url/search.pm
@@ -1,37 +1,4 @@
-package CIF::WebAPI::urls::searches;
+package CIF::WebAPI::urls::search;
 use base 'CIF::WebAPI';
-
-use strict;
-use warnings;
-
-use CIF::Message::UrlSearch;
-
-sub GET {
-    my ($self, $request, $response) = @_;
-
-    if(exists($self->{'address'})){
-        my $arg = $self->address();
-        my $col = 'address';
-        if($arg =~ /^[a-fA-F0-9]{32}$/){
-            $col = 'url_md5';
-        } elsif($arg =~ /^[a-fA-F0-9]{40}$/){
-            $col = 'url_sha1';
-        }
-        my @recs = CIF::Message::UrlSearch->search($col => $arg);
-        if(@recs){
-            my @res = map { CIF::WebAPI::urls::mapIndex($_) } @recs;
-            $response->data()->{'result'} = \@res;
-        }
-    }
-    return Apache2::Const::HTTP_OK;
-}
-
-sub buildNext {
-    my ($self,$frag,$req) = @_;
-    
-    my $h = CIF::WebAPI::urls::searches->new($self);
-    $h->{'address'} = $frag;
-    return $h;
-}
 
 1;
