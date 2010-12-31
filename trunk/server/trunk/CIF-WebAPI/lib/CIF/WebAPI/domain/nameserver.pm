847360251d859925fa4bbfe4a5a2976016e127c2
saxjazman9 <saxjazman9@gmail.com>
2010-12-31 19:48:53 +0000

    cleaning

--- trunk/server/trunk/CIF-WebAPI/lib/CIF/WebAPI/domain/nameserver.pm
+++ trunk/server/trunk/CIF-WebAPI/lib/CIF/WebAPI/domain/nameserver.pm
@@ -1,7 +1,4 @@
 package CIF::WebAPI::domain::nameserver;
 use base 'CIF::WebAPI';
 
-use strict;
-use warnings;
-
 1;
---
+++ trunk/server/trunk/CIF-WebAPI/lib/CIF/WebAPI/domain/whitelist.pm
@@ -0,0 +1,4 @@
+package CIF::WebAPI::domain::whitelist;
+use base 'CIF::WebAPI';
+
+1;
