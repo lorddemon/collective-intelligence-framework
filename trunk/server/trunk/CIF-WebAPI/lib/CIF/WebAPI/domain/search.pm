b05c8edf7f882286113290ee2dc56c68c2d67126
saxjazman9@gmail.com <saxjazman9@gmail.com@f9ffa795-1512-f4ca-611e-4b753ca244e6>
2010-12-31 19:44:52 +0000

    cleaning

--- trunk/server/trunk/CIF-WebAPI/lib/CIF/WebAPI/domain/search.pm
+++ trunk/server/trunk/CIF-WebAPI/lib/CIF/WebAPI/domain/search.pm
@@ -1,32 +1,4 @@
-package CIF::WebAPI::domain::searches;
+package CIF::WebAPI::domain::search;
 use base 'CIF::WebAPI';
-
-use strict;
-use warnings;
-
-use CIF::Message::DomainSearch;
-
-sub GET {
-    my ($self, $request, $response) = @_;
-
-    if(exists($self->{'domain'})){
-        my $arg = $self->domain();
-        my @recs = CIF::Message::DomainSearch->search_by_address('%'.$arg.'%',10);
-        if(@recs){
-            my @res = map { CIF::WebAPI::domain::mapIndex($_) } @recs;
-            $response->data()->{'result'} = \@res;
-        }
-    }
-    return Apache2::Const::HTTP_OK;
-
-}
-
-sub buildNext {
-    my ($self,$frag,$req) = @_;
-    
-    my $h = CIF::WebAPI::domain::searches->new($self);
-    $h->{'domain'} = $frag;
-    return $h;
-}
 
 1;
