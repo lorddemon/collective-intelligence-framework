ed4c8575c43c675c000e8dfb51b5c22f13fe9ace
saxjazman9 <saxjazman9@gmail.com>
2010-12-31 19:58:49 +0000

    cleaning

--- trunk/server/trunk/CIF-WebAPI/lib/CIF/WebAPI/email.pm
+++ trunk/server/trunk/CIF-WebAPI/lib/CIF/WebAPI/email.pm
@@ -4,59 +4,14 @@ use base 'CIF::WebAPI';
 use strict;
 use warnings;
 
-use CIF::Message::Email;
 use CIF::WebAPI::email::email;
-use CIF::WebAPI::email::cache;
-
-sub mapIndex {
-    my $r = shift;
-    my $idx = CIF::WebAPI::mapIndex($r);
-    delete($idx->{'rec'});
-
-    return {
-        %$idx,
-        address     => $r->address(),
-    };
-}
-
-sub aggregateFeed {
-    my @recs = @_;
-
-    my @res = @{CIF::WebAPI::aggregateFeed('address',@recs)};
-    my @feed;
-    foreach (@res){
-        my $idx = mapIndex($_->{'rec'});
-        delete($_->{'rec'});
-        push(@feed,$idx);
-    }
-    return(@feed);
-}
-
-sub generateFeed {
-    my $response = shift;
-    my @recs = @_;
-
-    my @feed = aggregateFeed(@recs);
-
-    $response->data()->{'result'} = \@feed;
-    return Apache2::Const::HTTP_OK;
-}
-
-sub GET {
-    my ($self, $request, $response) = @_;
-
-    my $detecttime = DateTime->from_epoch(epoch => (time() - (84600 * 30)));
-    my @recs = CIF::Message::Email->search_feed($detecttime,10000);
-    return generateFeed($response,@recs);
-}
 
 sub buildNext {
     my ($self,$frag,$req) = @_;
 
     if(uc($frag) =~ /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/){
-        my $subh = CIF::WebAPI::email::email->new($self);
-        $subh->{'address'} = $frag;
-        return $subh;
+        $self->{'query'} = $frag;
+        return $self;
     } else {
         return $self->SUPER::buildNext($frag,$req);
     }
