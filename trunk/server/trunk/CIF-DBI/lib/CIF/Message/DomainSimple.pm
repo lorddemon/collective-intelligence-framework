b6738922b7808dd96df0264f470b661c37fc84ba
saxjazman9 <saxjazman9@gmail.com>
2011-01-04 20:19:24 +0000

    refactored

--- trunk/server/trunk/CIF-DBI/lib/CIF/Message/DomainSimple.pm
+++ trunk/server/trunk/CIF-DBI/lib/CIF/Message/DomainSimple.pm
@@ -4,7 +4,6 @@ use base 'CIF::Message::Domain';
 use strict;
 use warnings;
 
-use CIF::Message::DomainWhitelist;
 use CIF::Message::Infrastructure;
 use CIF::Message::InfrastructureSimple;
 use CIF::Message::DomainMalware;
@@ -20,111 +19,87 @@ sub insert {
     my $self = shift;
     my $info = {%{+shift}};
 
-    my $domain = $info->{'address'};
-    return (undef,'invalid address: whitelisted') if(CIF::Message::Domain::isWhitelisted($domain));
+    return (undef,'invalid address: whitelisted') if(CIF::Message::Domain::isWhitelisted($info->{'address'}));
 
     my @ids;
-    my @results = CIF::Message::Domain::getrdata($info->{'nsres'},$domain);
-    foreach my $r (@results){
-        my $rdata;
-        my $description = $info->{'description'};
-        my $impact = $info->{'impact'};
-        my $severity = $info->{'severity'};
-        my $bucket = 'CIF::Message::DomainMalware';
+    my @results = CIF::Message::Domain::getrdata($info->{'nsres'},$info->{'address'});
 
-        if($r->{'address'}){
-            $rdata = $r->{'address'};
-            $description = $description.' '.$rdata if($rdata);
-            if($r->{'type'} eq 'NS'){
-                $severity = 'low';
-            }
-        }
-        if(lc($info->{'impact'} =~ /fastflux/)){
+    my $bucket = 'CIF::Message::DomainMalware';
+    my $impact = lc($info->{'impact'});
+    my $description = $info->{'description'};
+
+    for(lc($impact)){
+        if(/fastflux/){
             $bucket = 'CIF::Message::DomainFastflux';
-            $impact = 'fastflux domain';
-            $description = $impact;
-            $description .= ' '.$rdata if($rdata);
+            last;
         }
-        if(lc($info->{'impact'} =~ /nameserver/)){
+        if(/nameserver/){
             $bucket = 'CIF::Message::DomainNameserver';
-            $description = $impact.' '.$domain;
-            $description .= ' '.$rdata if($rdata);
+            last;
         }
-        if(lc($info->{'impact'} =~ /botnet/)){
+        if(/botnet/){
             $bucket = 'CIF::Message::DomainBotnet';
+            last;
         }
+    }
 
-        if($r->{'nameserver'}){
-            $bucket = 'CIF::Message::DomainNameserver';
-            $impact = 'suspicious nameserver';
-            $impact .= ' fastflux' if(lc($info->{'impact'}) =~ /fastflux/);
-            $description = $impact.' '.$rdata;
-            $severity = 'low';
-        }
-        if($r->{'cname'}){
-            $rdata = $r->{'cname'};
-            $description = $impact.' '.$domain.' '.$rdata;
-        }
-
-        my ($as,$network,$ccode,$rir,$date,$as_desc);
-        if($rdata && $rdata =~ /^$RE{net}{IPv4}/){
-            ($as,$network,$ccode,$rir,$date,$as_desc) = CIF::Message::Infrastructure::asninfo($rdata);
-        } else {
-            next if(CIF::Message::Domain::isWhitelisted($rdata));
-        }
-
-        my ($id,$err) = $bucket->insert({
-            relatedid   => $info->{'relatedid'},
-            address     => $domain,
-            source      => $info->{'source'},
-            confidence  => $info->{'confidence'},
-            severity    => $severity,
-            impact      => $impact,
-            description => $description,
-            detecttime  => $info->{'detecttime'},
-            class       => $r->{'class'},
-            ttl         => $r->{'ttl'},
-            type        => $r->{'type'},
-            rdata       => $rdata,
-            asn         => $as,
-            asn_desc    => $as_desc,
-            cidr        => $network,
-            cc          => $ccode,
-            rir         => $rir,
-            restriction => $info->{'restriction'},
-            alternativeid => $info->{'alternativeid'},
-            alternativeid_restriction => $info->{'alternativeid_restriction'},
-        });
+    foreach my $r (@results){
+        my %hash = %$info;
+        my ($id,$err) = $self->_insert($bucket,{%hash},$r);
         return(undef,$err) unless($id);
         push(@ids,$id);
-        my $confidence = ($info->{'confidence'}) ? ($info->{'confidence'} - 2) : 0;
-        $severity = ($severity eq 'high') ? 'medium' : 'low';
+    }
 
-        next if($r->{'type'} eq 'CNAME');
-        next unless($rdata && $rdata =~ /^$RE{net}{IPv4}/);
-        next if(CIF::Message::Infrastructure::isPrivateAddress($rdata));
-        next if(CIF::Message::Infrastructure::isWhitelisted($rdata));
+    # do the same for the nameservers
+    $info->{'severity'} = ($info->{'severity'} eq 'high') ? 'medium' : 'low';
+    $info->{'confidence'} = ($info->{'confidence'}) ? ($info->{'confidence'} - 2) : 0;
+    foreach my $r (grep { $_->{'type'} eq 'NS' } @results){
+        my @recs = CIF::Message::Domain::getrdata($info->{'nsres'},$r->{'nsdname'});
+        foreach my $rec (grep { $_->{'type'} eq 'A' } @recs){
+            my %hash = %$info;
+            $hash{'impact'} = 'suspicious nameserver';
+            $hash{'description'} = 'suspicious nameserver '.$info->{'impact'}.' '.$info->{'address'};
+
+            my ($id,$err) = $self->_insert('CIF::Message::DomainNameserver',{%hash},$rec);
+            return(undef,$err) unless($id);
+            push(@ids,$id);
+        }
+    }
+    return($ids[0]);
+}
+
+sub _insert {
+    my ($self,$b,$h,$r) = @_;
+    my %hash = %$h;
+
+    $hash{'address'} = $r->{'name'};
+    my $rdata = $r->{'address'} || $r->{'cname'} || $r->{'ptrdname'} || $r->{'nsdname'} || $r->{'exchange'};
+    my ($as,$network,$ccode,$rir,$date,$as_desc);
+    if($rdata && $rdata =~ /^$RE{net}{IPv4}/){
+        ($as,$network,$ccode,$rir,$date,$as_desc) = CIF::Message::Infrastructure::asninfo($rdata);
+    } else {
+        next if(CIF::Message::Domain::isWhitelisted($rdata));
+    }
+    $hash{'rdata'}  = $rdata;
+    $hash{'type'}   = $r->{'type'};
+    $hash{'class'}  = $r->{'class'};
+    $hash{'ttl'}    = $r->{'ttl'};
+
+    my ($id,$err) = $b->insert({%hash});
+    return(undef,$err) unless($id);
+
+    $hash{'severity'} = ($hash{'severity'} eq 'high') ? 'medium' : 'low';
+    $hash{'confidence'} = ($hash{'confidence'}) ? ($hash{'confidence'} - 2) : 0;
+    $hash{'detecttime'} = DateTime->from_epoch(epoch => time());
+
+    if($rdata && $rdata =~ /^$RE{net}{IPv4}/){
+        $hash{'address'} = $hash{'rdata'};
         CIF::Message::InfrastructureSimple->insert({
             relatedid   => $id->uuid(),
-            address     => $rdata,
-            impact      => $impact,
-            source      => $info->{'source'},
-            description => $description,
-            confidence  => $confidence,
-            severity    => $severity,
-            detecttime  => DateTime->from_epoch(epoch => time()),
-            restriction => $info->{'restriction'},
-            asn         => $as,
-            asn_desc    => $as_desc,
-            cidr        => $network,
-            cc          => $ccode,
-            rir         => $rir,
-            cidr        => $network,
-            alternativeid   => $info->{'alternativeid'},
-            alternativeid_restriction => $info->{'alternativeid_restriction'},
+            %hash,
         });
     }
-    return($ids[$#ids]);
+    return($id);
 }
 
 1;
