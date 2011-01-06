4a822cc27d6523cde0a120f0981f437f4fa564ec
saxjazman9 <saxjazman9@gmail.com>
2011-01-06 16:17:43 +0000

    adding pcapfilter plugin

---
+++ trunk/client/trunk/perl/CIF-Client/lib/CIF/Client/Plugin/Pcapfilter.pm
@@ -0,0 +1,21 @@
+package CIF::Client::Plugin::Pcapfilter;
+
+use Regexp::Common qw/net/;
+use Regexp::Common::net::CIDR;
+
+sub write_out {
+    my $self = shift;
+    my @array = @_;
+    my $text = '';
+    foreach (@array){
+        my $address = $_->{'address'};
+        if($address =~ /^$RE{'net'}{'CIDR'}{'IPv4'}$/){
+            $text .= "net $address or ";
+        } else {
+            $text .= "host $address or ";
+        }
+    }
+    $text =~ s/ or//;
+    return $text;
+}
+1;
