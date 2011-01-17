612e3a8cf0f552719e05e88801844c0124fe2df7
saxjazman9 <saxjazman9@gmail.com>
2011-01-17 15:22:18 +0000

    adding files

---
+++ trunk/server/trunk/CIF-DBI/lib/CIF/Message/FeedInfrastructureSearch.pm
@@ -0,0 +1,9 @@
+package CIF::Message::FeedInfrastructureSearch;
+use base 'CIF::Message::FeedInfrastructure';
+
+use strict;
+use warnings;
+
+__PACKAGE__->table('feed_infrastructure_search');
+
+1;
