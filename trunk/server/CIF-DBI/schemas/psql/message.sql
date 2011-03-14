e171866197a177289cb7a09b0f3e683d266c4352
saxjazman9 <saxjazman9@gmail.com>
2011-03-14 18:57:20 +0000

    saving

--- trunk/server/CIF-DBI/schemas/psql/message.sql
+++ trunk/server/CIF-DBI/schemas/psql/message.sql
@@ -5,37 +5,27 @@ CREATE TYPE restriction AS ENUM ('public','need-to-know','private','default');
 CREATE TABLE message (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     uuid uuid NOT NULL,
-    source uuid NOT NULL,
-    type VARCHAR(16) not null,
-    format VARCHAR(32), -- IODEF, MetaSharing, IRC, Email, etc...
-    confidence REAL,
-    severity severity,
+    source uuid,
+    format text, -- IODEF, MetaSharing, IRC, Email, etc...
     description text,
-    impact VARCHAR(140),
-    restriction restriction not null default 'private',
-    detecttime timestamp with time zone DEFAULT NOW(),
+    restriction restriction default 'private',
     created timestamp with time zone DEFAULT NOW(),
+    message text not null,
     UNIQUE (uuid)
 );
 
-CREATE TABLE message_structured (
-    id BIGSERIAL PRIMARY KEY NOT NULL,
-    uuid uuid REFERENCES message (uuid) ON DELETE CASCADE NOT NULL,
-    source uuid NOT NULL,
-    message xml,
-    UNIQUE(uuid)
-);
+-- unclear if we really need this for xpath queries or not
 
-CREATE TABLE message_unstructured (
-    id BIGSERIAL PRIMARY KEY NOT NULL,
-    uuid uuid REFERENCES message (uuid) ON DELETE CASCADE NOT NULL,
-    source uuid NOT NULL,
-    message text NOT NULL,
-    UNIQUE(uuid)
-);
-
-CREATE VIEW v_message AS
-SELECT message.*,message_unstructured.message as unstructured, message_structured.message as structured
-FROM message 
-LEFT JOIN message_unstructured ON (message_unstructured.uuid = message.uuid AND message_unstructured.source = message.source)
-LEFT JOIN message_structured ON (message_structured.uuid = message.uuid AND message_structured.source = message.source);
+-- DROP TABLE IF EXISTS message_xml CASCADE;
+-- CREATE TABLE message_xml (
+--    id bigserial primary key not null,
+--   uuid uuid references message(uuid) on delete cascade,
+--    source uuid,
+--    format text,
+--    description text,
+--    restriction restriction default 'private',
+--   message xml,
+--    detecttime timestamp with time zone default now(),
+--    created timestamp with time zone default now(),
+--    UNIQUE(uuid)
+--);
