2d55ab6c9dddb198ca6345a24b6ff48e15af7000
saxjazman9@gmail.com <saxjazman9@gmail.com@f9ffa795-1512-f4ca-611e-4b753ca244e6>
2011-04-15 12:38:16 +0000

    splitting up CIF::WebAPI into plugins

---
+++ trunk/server/CIF-WebAPI-Core/lib/CIF/WebAPI/Core.pm
@@ -0,0 +1,64 @@
+package CIF::WebAPI::Core;
+
+use 5.008008;
+use strict;
+use warnings;
+
+our $VERSION = '0.00_01';
+$VERSION = eval $VERSION;  # see L<perlmodstyle>
+
+
+# Preloaded methods go here.
+
+1;
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+CIF::WebAPI::Core - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use CIF::WebAPI::Core;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for CIF::WebAPI::Core, created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head2 EXPORT
+
+None by default.
+
+
+
+=head1 SEE ALSO
+
+Mention other useful documentation such as the documentation of
+related modules or operating system documentation (such as man pages
+in UNIX), or any relevant external documentation such as RFCs or
+standards.
+
+If you have a mailing list set up for your module, mention it here.
+
+If you have a web site set up for your module, mention it here.
+
+=head1 AUTHOR
+
+Wes Young, E<lt>wes@E<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011 by Wes Young
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
