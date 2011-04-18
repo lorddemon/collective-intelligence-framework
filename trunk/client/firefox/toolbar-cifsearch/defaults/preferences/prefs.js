aaa27d67b68c9e447a3bc5b6e2bce5a1d0512c57
saxjazman9@gmail.com <saxjazman9@gmail.com@f9ffa795-1512-f4ca-611e-4b753ca244e6>
2011-04-18 14:06:42 +0000

    replacing label with a default placeholder of search cif

--- trunk/client/firefox/toolbar-cifsearch/defaults/preferences/prefs.js
+++ trunk/client/firefox/toolbar-cifsearch/defaults/preferences/prefs.js
@@ -1,5 +1,5 @@
 pref("extensions.cifsearch.strapikey","xx-xx-xx-xx-xx");
-pref("extensions.cifsearch.strhost","https://example.com/api");
+pref("extensions.cifsearch.strhost","https://example.com:443/api");
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.cif-0.01_01@barely3am.com.description", "chrome://cifsearch/locale/overlay.properties");
