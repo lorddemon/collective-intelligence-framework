9b43c96f7f2e332a1ed37bd8aee01b241cf098b7
saxjazman9@gmail.com <saxjazman9@gmail.com@f9ffa795-1512-f4ca-611e-4b753ca244e6>
2011-04-26 16:56:43 +0000

    adding url index plugin

---
+++ trunk/server/CIF-Archive-DataType-Plugin-Url/lib/CIF/Archive/DataType/Plugin/Url/Phishing.pm
@@ -0,0 +1,12 @@
+package CIF::Archive::DataType::Plugin::Url::Phishing;
+use base 'CIF::Archive::DataType::Plugin::Url';
+
+sub prepare {
+    my $class = shift;
+    my $info = shift;
+    return(0) unless($info->{'impact'} =~ /phish/);
+    return('url_phishing');
+}
+
+1;
+
