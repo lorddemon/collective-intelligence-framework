8e9b4b6f8876cc31f36d63593a2e20a2e0e4d24f
saxjazman9 <saxjazman9@gmail.com>
2011-04-28 16:10:42 +0000

    minor bugfixes

--- trunk/server/CIF-Archive-DataType-Plugin-Domain/schemas/psql/main.sql
+++ trunk/server/CIF-Archive-DataType-Plugin-Domain/schemas/psql/main.sql
@@ -23,45 +23,45 @@ CREATE TABLE domain (
     restriction restriction not null default 'private',
     detecttime timestamp with time zone DEFAULT NOW(),
     created timestamp with time zone DEFAULT NOW(),
-    UNIQUE (uuid)
+    UNIQUE (uuid,address,rdata)
 );
 
 CREATE TABLE domain_whitelist() INHERITS (domain);
 ALTER TABLE domain_whitelist ADD PRIMARY KEY (id);
 ALTER TABLE domain_whitelist ADD CONSTRAINT domain_whitelist_uuid_fkey FOREIGN KEY (uuid) REFERENCES archive(uuid) ON DELETE CASCADE;
-ALTER TABLE domain_whitelist ADD UNIQUE(uuid);
+ALTER TABLE domain_whitelist ADD UNIQUE(uuid,address,rdata);
 
 CREATE TABLE domain_fastflux() INHERITS (domain);
 ALTER TABLE domain_fastflux ADD PRIMARY KEY (id);
 ALTER TABLE domain_fastflux ADD CONSTRAINT domain_fastflux_uuid_fkey FOREIGN KEY (uuid) REFERENCES archive(uuid) ON DELETE CASCADE;
-ALTER TABLE domain_fastflux ADD UNIQUE(uuid);
+ALTER TABLE domain_fastflux ADD UNIQUE(uuid,address,rdata);
 
 CREATE TABLE domain_nameserver() INHERITS (domain);
 ALTER TABLE domain_nameserver ADD PRIMARY KEY (id);
 ALTER TABLE domain_nameserver ADD CONSTRAINT domain_nameserver_uuid_fkey FOREIGN KEY (uuid) REFERENCES archive(uuid) ON DELETE CASCADE;
-ALTER TABLE domain_nameserver ADD UNIQUE(uuid);
+ALTER TABLE domain_nameserver ADD UNIQUE(uuid,address,rdata);
 
 CREATE TABLE domain_malware() INHERITS (domain);
 ALTER TABLE domain_malware ADD PRIMARY KEY (id);
 ALTER TABLE domain_malware ADD CONSTRAINT domain_malware_uuid_fkey FOREIGN KEY (uuid) REFERENCES archive(uuid) ON DELETE CASCADE;
-ALTER TABLE domain_malware ADD UNIQUE(uuid);
+ALTER TABLE domain_malware ADD UNIQUE(uuid,address,rdata);
 
 CREATE TABLE domain_botnet() INHERITS (domain);
 ALTER TABLE domain_botnet ADD PRIMARY KEY (id);
 ALTER TABLE domain_botnet ADD CONSTRAINT domain_botnet_uuid_fkey FOREIGN KEY (uuid) REFERENCES archive(uuid) ON DELETE CASCADE;
-ALTER TABLE domain_botnet ADD UNIQUE(uuid);
+ALTER TABLE domain_botnet ADD UNIQUE(uuid,address,rdata);
 
 CREATE TABLE domain_passivedns() INHERITS (domain);
 ALTER TABLE domain_passivedns ADD PRIMARY KEY (id);
 ALTER TABLE domain_passivedns ADD CONSTRAINT domain_passivedns_uuid_fkey FOREIGN KEY (uuid) REFERENCES archive(uuid) ON DELETE CASCADE;
-ALTER TABLE domain_passivedns ADD UNIQUE(uuid);
+ALTER TABLE domain_passivedns ADD UNIQUE(uuid,address,rdata);
 
 CREATE TABLE domain_search() INHERITS (domain);
 ALTER TABLE domain_search ADD PRIMARY KEY (id);
 ALTER TABLE domain_search ADD CONSTRAINT domain_search_uuid_fkey FOREIGN KEY (uuid) REFERENCES archive(uuid) ON DELETE CASCADE;
-ALTER TABLE domain_search ADD UNIQUE(uuid);
+ALTER TABLE domain_search ADD UNIQUE(uuid,address,rdata);
 
 CREATE TABLE domain_phishing() INHERITS (domain);
 ALTER TABLE domain_phishing ADD PRIMARY KEY (id);
 ALTER TABLE domain_phishing ADD CONSTRAINT domain_phishing_uuid_fkey FOREIGN KEY (uuid) REFERENCES archive(uuid) ON DELETE CASCADE;
-ALTER TABLE domain_phishing ADD UNIQUE(uuid);
+ALTER TABLE domain_phishing ADD UNIQUE(uuid,address,rdata);
