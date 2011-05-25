bed1215eaaec873fc2dd169f1cb5ed64fd9c87b0
saxjazman9@gmail.com <saxjazman9@gmail.com@f9ffa795-1512-f4ca-611e-4b753ca244e6>
2011-05-25 00:32:16 +0000

    bugfix

--- trunk/server/CIF-Archive-Storage-Plugin-Iodef/lib/CIF/Archive/Storage/Plugin/Iodef/Url.pm
+++ trunk/server/CIF-Archive-Storage-Plugin-Iodef/lib/CIF/Archive/Storage/Plugin/Iodef/Url.pm
@@ -1,16 +1,19 @@
 package CIF::Archive::Storage::Plugin::Iodef::Url;
 
 use Regexp::Common qw /URI/;
+use URI::Escape;
 
 sub prepare {
     my $class   = shift;
     my $info    = shift;
 
     my $address = $info->{'address'};
-    return(0) unless($address);
-    return(1) if($address =~ /$RE{URI}{HTTP}/);
-    return(0);
+    return unless($address);
+    return unless($address =~ /^$RE{'URI'}/);
+    return(1);
 }
+
+sub from {};
 
 sub convert {
     my $class = shift;
@@ -18,10 +21,35 @@ sub convert {
     my $iodef = shift;
 
     my $address = lc($info->{'address'});
+    my $md5 = $info->{'md5'};
+    my $sha1 = $info->{'sha1'};
 
     $iodef->add('IncidentEventDataFlowSystemNodeAddresscategory','ext-value');
     $iodef->add('IncidentEventDataFlowSystemNodeAddressext-category','url');
     $iodef->add('IncidentEventDataFlowSystemNodeAddress',$address);
+
+    if($md5){
+        $iodef->add('IncidentEventDataFlowSystemAdditionalDatadtype','string');
+        $iodef->add('IncidentEventDataFlowSystemAdditionalDatameaning','md5');
+        $iodef->add('IncidentEventDataFlowSystemAdditionalData',$md5);
+    }
+    if($sha1){
+        $iodef->add('IncidentEventDataFlowSystemAdditionalDatadtype','string');
+        $iodef->add('IncidentEventDataFlowSystemAdditionalDatameaning','sha1');
+        $iodef->add('IncidentEventDataFlowSystemAdditionalData',$sha1);
+    }
+
+    if($info->{'malware_md5'} || $info->{'malware_sha1'}){
+        require CIF::Archive::Storage::Plugin::Iodef::Malware;
+        $iodef = CIF::Archive::Storage::Plugin::Iodef::Malware->convert({
+            source  => $info->{'source'},
+            md5     => $info->{'malware_md5'},
+            sha1    => $info->{'malware_sha1'},
+            impact  => $info->{'impact'},
+            description => $info->{'description'},
+            detecttime  => $info->{'detecttime'},
+        },$iodef);
+    }
     return($iodef);
 }
 
