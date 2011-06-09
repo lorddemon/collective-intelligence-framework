d70134a810e8ce1cd23543d2a58d662ca8cbf215
saxjazman9@gmail.com <saxjazman9@gmail.com@f9ffa795-1512-f4ca-611e-4b753ca244e6>
2011-06-09 13:02:59 +0000

    saving

--- trunk/server/CIF-Archive-Storage-Plugin-Iodef/lib/CIF/Archive/Storage/Plugin/Iodef/Email.pm
+++ trunk/server/CIF-Archive-Storage-Plugin-Iodef/lib/CIF/Archive/Storage/Plugin/Iodef/Email.pm
@@ -1,12 +1,28 @@
 package CIF::Archive::Storage::Plugin::Iodef::Email;
 
+use Regexp::Common qw/URI/;
+
 sub prepare {
     my $class   = shift;
     my $info    = shift;
 
     my $address = $info->{'address'};
-    return(0) unless($address);
-    return(1) if($address =~ /\w+@\w+/);
+    return unless($address);
+    return if($address =~ /^$RE{'URI'}/);
+    return unless($address =~ /\w+@\w+/);
+    return(1);
+}
+
+sub data_hash_simple {
+    my $class = shift;
+    my $hash = shift;
+
+    my $address = $hash->{'EventData'}->{'Flow'}->{'System'}->{'Node'}->{'Address'};
+    $address = $address->{'content'} if(ref($address) eq 'HASH');
+
+    return({
+        address => $address,
+    });
 }
 
 sub convert {
