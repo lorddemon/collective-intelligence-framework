311ac6a602475fabab04f5945593b4253f7813a3
saxjazman9@gmail.com <saxjazman9@gmail.com@f9ffa795-1512-f4ca-611e-4b753ca244e6>
2011-06-09 12:52:11 +0000

    version bump, released rino-0.01

--- trunk/client/perl/RINO-Client/lib/RINO/Client.pm
+++ trunk/client/perl/RINO-Client/lib/RINO/Client.pm
@@ -4,7 +4,7 @@ use 5.008008;
 use strict;
 use warnings;
 
-our $VERSION = '0.00_01';
+our $VERSION = '0.01';
 $VERSION = eval $VERSION;  # see L<perlmodstyle>
 
 use Module::Pluggable require => 1;
