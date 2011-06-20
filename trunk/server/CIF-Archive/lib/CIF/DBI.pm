ceae6d5b9d9c781cbe393902539a1cdc84ce6d17
saxjazman9@gmail.com <saxjazman9@gmail.com@f9ffa795-1512-f4ca-611e-4b753ca244e6>
2011-06-20 14:58:04 +0000

    saving

--- trunk/server/CIF-Archive/lib/CIF/DBI.pm
+++ trunk/server/CIF-Archive/lib/CIF/DBI.pm
@@ -4,6 +4,21 @@ use base 'Class::DBI';
 use Config::Simple;
 
 __PACKAGE__->connection('DBI:Pg:database=cif;host=localhost','postgres','',{ AutoCommit => 1} );
+__PACKAGE__->set_sql('lastval' => qq{ SELECT last_value from archive_id_seq });
+
+# because UUID's are really primary keys too in our schema
+# this overrides some of the default functionality of Class::DBI and 'id'
+sub retrieve {
+    my $class = shift;
+    my %keys = @_;
+
+    return $class->SUPER::retrieve(@_) unless($keys{'uuid'});
+
+    require CIF::Archive;
+    my @recs = CIF::Archive->search(uuid => $keys{'uuid'});
+    return unless(@recs);
+    return($recs[0]);
+}
 
 1;
 
