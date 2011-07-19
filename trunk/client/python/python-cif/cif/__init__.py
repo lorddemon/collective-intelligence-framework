07c09a229fd335a8d262a21adce8c2d1d4dd0d51
saxjazman9 <saxjazman9@gmail.com>
2011-07-19 18:25:33 +0000

    initial restructuring for server 0.01

--- trunk/client/python/python-cif/cif/__init__.py
+++ trunk/client/python/python-cif/cif/__init__.py
@@ -9,8 +9,6 @@ import zlib
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
-#version = '0.00_06'
-
 class Client(object):
     def __init__(self, host, apikey, fields=None, severity=None, restriction=None, nolog=None, **args):
         self.host = host
@@ -31,7 +29,7 @@ class Client(object):
         self._fields = fields
     fields = property(_get_fields, _set_fields)
     
-    def GET(self,q,severity=None,restriction=None,nolog=None):
+    def GET(self,q,severity=None,restriction=None,nolog=None,confidence=None):
         s = self.host + '/' + q
         
         params={'apikey':self.apikey}
@@ -46,6 +44,11 @@ class Client(object):
         elif self.severity:
             params['severity'] = self.severity
 
+        if confidence:
+            params['confidence'] = confidence
+        elif self.confidence:
+            params['confidence'] = self.confidence
+
         if nolog:
             params['nolog'] = 1
         elif self.nolog:
@@ -59,6 +62,10 @@ class Client(object):
 
         """ check to see if we've got a feed object, auto-de(code/compress) it """
         feed = ret['data']
+
+        if not feed.get('feed'):
+            return
+
         entry = feed['feed']['entry']
         if type(entry[0]) == str:
             jstring = zlib.decompress(b64decode(entry[0]))
@@ -69,6 +76,67 @@ class Client(object):
         #self.responseContent = json.dumps(ret)
         return feed
 
+    def simple(self,incident):
+            ret = {}
+            impact = incident['Assessment']['Impact']
+            ret['restriction'] = incident['restriction']
+            ret['purpose'] = incident['purpose']
+            ret['detecttime'] = incident['DetectTime']
+            if(incident['IncidentID'].has_key('content')):
+                ret['uuid'] = incident['IncidentID']['content']
+            ret['source'] = incident['IncidentID']['name']
+            ret['description'] = incident['Description']
+            ret['confidence'] = incident['Assessment']['Confidence']['content']
+
+            if(incident['EventData'].has_key('AdditionalData')):
+                data = incident['EventData']['AdditionalData']
+                dlist = [] 
+                if(type(data) == dict):
+                    dlist.extend([data])
+                else:
+                    dlist = data
+
+                for d in dlist:
+                    if d['meaning'] == 'malware_md5':
+                        ret['malware_md5'] = d['content']
+                    if d['meaning'] == 'malware_sha1':
+                        ret['malware_md5'] = d['content']
+                    if d['meaning'] == 'md5':
+                        ret['md5'] = d['content']
+                    if d['meaning'] == 'sha1':
+                        ret['sha1'] = d['content']
+
+            if(incident.has_key('RelatedActvity')):
+                ret['relatedid'] = incident['RelatedActivity']['IncidentID']
+
+            if(incident.has_key('AlternativeID')):
+                if incident['AlternativeID']['IncidentID'].has_key('content'):
+                    ret['alternativeid'] = incident['AlternativeID']['IncidentID']['content']
+                    ret['alternativeid_restriction'] = incident['AlternativeID']['IncidentID']['restriction']
+                else:
+                    ret['alternativeid'] = None
+                    ret['alternativeid_restriction'] = None
+
+            ret['impact'] = impact
+            ret['severity'] = None
+            if(type(impact) == dict):
+                severity = incident['Assessment']['Impact']['severity']
+                impact = impact['content']
+                ret['severity'] = severity
+                ret['impact'] = impact
+
+            if(incident['EventData'].has_key('Flow')):
+                system = incident['EventData']['Flow']['System']
+                ret['address'] = system['Node']['Address']
+                if(type(ret['address']) == dict):
+                    ret['address'] = ret['address']['content']
+                if(system.has_key('Service')):
+                    ret['protocol'] = system['Service']['ip_protocol']
+                    ret['portlist'] = system['Service']['Portlist']
+
+            return ret
+                
+
     def table(self,feed):
         """
         Take in the JSON object and print a neat table out of the data
@@ -78,34 +146,49 @@ class Client(object):
         feed -- feed dict
         """
         
-        j = feed
-        created = j.get('created') 
+        j = feed.get('feed')
+        created = j.get('detecttime') 
         feedid = j.get('id')
-        restriction = j['feed'].get('restriction')
-        severity = j['feed'].get('severity')
-
-        feed = j['feed']['entry']
+        restriction = j.get('restriction')
+        entries = j['entry']
+        severity = j.get('severity')
         
         t = Texttable(max_width=0)
         t.set_deco(Texttable.VLINES)
+        dlist = []
+        for incident in entries:
+            if(type(incident) == dict):
+                x = self.simple(incident['Incident'])
+                dlist.extend([x])
+            else:
+                for i in incident:
+                    x = self.simple(i['Incident'])
+                    dlist.extend(x)
 
         if self.fields:
             cols = self.fields
         else:
             cols = ['restriction','severity']
-            if feed[0].has_key('rdata'):
-                cols.extend(['address','rdata','type'])
-            elif feed[0].has_key('hash_md5'):
-                cols.extend(['hash_md5','hash_sha1'])
-            elif feed[0].has_key('url_md5'):
-                cols.extend(['url_md5','url_sha1','malware_md5','malware_sha1'])
-            else:
-                cols.extend(['address','portlist'])
+            if dlist[0].has_key('address'):
+                cols.extend(['address','protocol','portlist'])
+
+            if dlist[0].has_key('md5'):
+                cols.extend(['md5'])
+
+            if dlist[0].has_key('sha1'):
+                cols.extend('sha1')
+
+            if dlist[0].has_key('malware_md5'):
+                cols.extend(['malware_md5'])
+
+            if dlist[0].has_key('malware_sha1'):
+                cols.extend(['malware_sha1'])
+
 
             cols.extend(['detecttime','description','alternativeid_restriction','alternativeid'])
         
         t.add_row(cols)
-        for item in feed:
+        for item in dlist:
             for col in cols:
                 if isinstance(item[col],unicode):
                     item[col] = item[col].encode('utf-8')
@@ -129,7 +212,7 @@ class Client(object):
         return table
 
 class ClientINI(Client):
-    def __init__(self, path=None, fields=None, severity=None, restriction=None, nolog=None):
+    def __init__(self, path=None, fields=None, severity=None, restriction=None, nolog=None, confidence=None):
         if not path:
             path = os.path.expanduser("~/.cif")
         c = ConfigParser.ConfigParser()
--- trunk/client/python/python-cif/scripts/cifcli.py
+++ trunk/client/python/python-cif/scripts/cifcli.py
@@ -3,19 +3,22 @@
 import cif
 import argparse
 import os
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 if __name__ == '__main__':
     # Parse Command Line Arguments
     parser = argparse.ArgumentParser(description="Command line interface to CIF APIs")
 
     parser.add_argument("-q",'--query', nargs='*',metavar="QUERY")
-    parser.add_argument('-s','--severity',help="specify the default severity")
-    parser.add_argument('-r','--restriction')
+    parser.add_argument('-s','--severity',help="specify the default severity",default='high')
+    parser.add_argument('-c','--confidence',help="specify the default confidence",default='85')
+    parser.add_argument('-r','--restriction',help='specify the default restriction',default='need-to-know')
     parser.add_argument("-f", '--fields',nargs='*',metavar="FIELD")
-    parser.add_argument("-c","--config",default=os.path.expanduser("~/.cif"))
+    parser.add_argument("-C","--config",default=os.path.expanduser("~/.cif"))
     parser.add_argument("-n","--nolog",action="store_true",default=False,help="do not log the query on the server")
     args = parser.parse_args()
-    print args
+    #print args
 
     if not args.query:
         parser.print_help()
@@ -30,11 +33,12 @@ if __name__ == '__main__':
     for query in args.query:
         # this returns a dict
         # need to translate it to an object with "plugin" type properties
-        feed = rclient.GET(query,args.severity,args.restriction,args.nolog)
+        feed = rclient.GET(query,args.severity,args.restriction,args.nolog,args.confidence)
         if rclient.responseCode != 200:
             print 'request failed with code: ' + str(rclient.responseCode)
             os._exit(-1)
 
-        print "Query: " + query
-        text = rclient.table(feed)
-        print text
+        if feed:
+            print "Query: " + query
+            text = rclient.table(feed)
+            print text
