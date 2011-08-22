a6a825b4e18ee64944d7216e71eba8a81430b032
saxjazman9@gmail.com <saxjazman9@gmail.com@f9ffa795-1512-f4ca-611e-4b753ca244e6>
2011-08-22 13:14:59 +0000

    bugfix

--- branches/wes-groups/client/perl/CIF-Client/lib/CIF/Client/Plugin/Raw.pm
+++ branches/wes-groups/client/perl/CIF-Client/lib/CIF/Client/Plugin/Raw.pm
@@ -10,12 +10,14 @@ sub write_out {
     my $json;
     if(1 || $config->{'stream'}){
         my @array = @{$feed->{'feed'}->{'entry'}};
+        return unless(keys(%{$array[0]}));
         my @json_stream;
         foreach(@array){
             push(@json_stream,JSON::to_json($_));
         }
         $json = join("\n",@json_stream);
     } else {
+        return unless($feed->{'feed'}->{'entry'});
         $json = JSON::to_json($feed->{'feed'}->{'entry'});
     }
     return $json."\n";
