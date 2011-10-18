f609b2eaaaa9a739262dd881abb9b68f1bb4f762
saxjazman9@gmail.com <saxjazman9@gmail.com@f9ffa795-1512-f4ca-611e-4b753ca244e6>
2011-10-18 13:36:02 +0000

    bugfix

--- trunk/client/python/python-cif/setup.py
+++ trunk/client/python/python-cif/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='cif',
-    version='0.01_08',
+    version='0.01_09',
     description="a sample python client for accessing the CIF interface",
     author = 'Wes Young',
     author_email = 'ci-framework@googlegroups.com',
 #    url = 'http://code.google.com/p/collective-intelligence-framework/',
-    license = 'BSD',
+    license = 'GPLv2',
     packages=find_packages(),
     zip_safe = False,
     install_requires= ['httplib2 >= 0.7.1','simplejson >= 2.1.1','Texttable','argparse'],
