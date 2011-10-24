85380bceb0326557ce1a4309e237638e3e2e9eb6
saxjazman9 <saxjazman9@gmail.com>
2011-10-24 12:58:56 +0000

    bugfix

--- trunk/server/lib/CIF/Archive/Analytic/Plugin/ResolveUrl.pm
+++ trunk/server/lib/CIF/Archive/Analytic/Plugin/ResolveUrl.pm
@@ -19,10 +19,11 @@ sub process {
     my $port;
     ## todo -- fix this, it flags on things like trainer.exe
     ## test with malc0de specifically
-    if($a =~ /^(https?\:\/\/)?([A-Za-z0-9-\.]+\.[a-z]{2,5})(:\d+)?\//){
+    warn $a;
+    if($a =~ /^(https?\:\/\/)?([A-Za-z0-9-.]+\.[a-z]{2,5})(:\d+)?(\/)?/){
         $address = $2;
         $port = $3;
-    } elsif($a =~ /^(https?\:\/\/)?($RE{'net'}{'IPv4'})(:\d+)?\//) {
+    } elsif($a =~ /^(https?\:\/\/)?($RE{'net'}{'IPv4'})(:\d+)?(\/)?/) {
         $address = $2;
         $port = $3;
         $port = '443' unless($port);
