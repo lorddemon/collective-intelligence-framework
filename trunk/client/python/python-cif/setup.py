d7708013307f3266dc580fd7eea01a807c9e9a1b
saxjazman9@gmail.com <saxjazman9@gmail.com@f9ffa795-1512-f4ca-611e-4b753ca244e6>
2011-10-28 15:08:39 +0000

    braindead bugfixes....

--- trunk/client/python/python-cif/setup.py
+++ trunk/client/python/python-cif/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cif',
-    version='0.01_10',
+    version='0.01_12',
     description="a sample python client for accessing the CIF interface",
     author = 'Wes Young',
     author_email = 'ci-framework@googlegroups.com',
@@ -10,7 +10,7 @@ setup(
     license = 'GPLv2',
     packages=find_packages(),
     zip_safe = False,
-    install_requires= ['httplib2 >= 0.7.1','simplejson >= 2.1.1','Texttable','argparse','SocksiPy >= 1.0'],
+    install_requires= ['httplib2 >= 0.7.1','simplejson >= 2.1.1','Texttable','argparse','SocksiPy >= 1.0-1'],
     ## TODO -- change this to entry points instead of scripts
     scripts = [
         'scripts/cifcli.py'
