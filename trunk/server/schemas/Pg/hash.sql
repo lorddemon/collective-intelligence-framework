88fad4715bde6f7a7e9fbc0e0e104bce67abb748
saxjazman9@gmail.com <saxjazman9@gmail.com@f9ffa795-1512-f4ca-611e-4b753ca244e6>
2011-11-04 16:42:22 +0000

    adding new table indicies

--- trunk/server/schemas/Pg/hash.sql
+++ trunk/server/schemas/Pg/hash.sql
@@ -20,18 +20,21 @@ ALTER TABLE hash_md5 ADD PRIMARY KEY (id);
 ALTER TABLE hash_md5 ADD CONSTRAINT hash_md5_uuid_fkey FOREIGN KEY (uuid) REFERENCES archive(uuid) ON DELETE CASCADE;
 ALTER TABLE hash_md5 ADD UNIQUE(uuid);
 ALTER TABLE hash_md5 ALTER COLUMN type SET DEFAULT 'md5';
-CREATE INDEX idx_feed_hash_md5 ON hash_md5 (detecttime DESC, severity DESC, confidence DESC);
+CREATE INDEX idx_feed_hash_md5 ON hash_md5 (detecttime DESC, severity DESC, confidence DESC, restriction DESC);
+CREATE INDEX idx_query_hash_md5 ON hash_md5 (lower(hash), detecttime DESC, severity DESC, confidence DESC, restriction DESC);
 
 CREATE TABLE hash_sha1 () INHERITS (hash);
 ALTER TABLE hash_sha1 ADD PRIMARY KEY (id);
 ALTER TABLE hash_sha1 ADD CONSTRAINT hash_sha1_uuid_fkey FOREIGN KEY (uuid) REFERENCES archive(uuid) ON DELETE CASCADE;
 ALTER TABLE hash_sha1 ADD UNIQUE(uuid);
 ALTER TABLE hash_sha1 ALTER COLUMN type SET DEFAULT 'sha1';
-CREATE INDEX idx_feed_hash_sha1 ON hash_sha1 (detecttime DESC, severity DESC, confidence DESC);
+CREATE INDEX idx_feed_hash_sha1 ON hash_sha1 (detecttime DESC, severity DESC, confidence DESC, restriction DESC);
+CREATE INDEX idx_query_hash_sha1 ON hash_sha1 (lower(hash), detecttime DESC, severity DESC, confidence DESC, restriction DESC);
 
 CREATE TABLE hash_uuid () INHERITS (hash);
 ALTER TABLE hash_uuid ADD PRIMARY KEY (id);
 ALTER TABLE hash_uuid ADD CONSTRAINT hash_uuid_uuid_fkey FOREIGN KEY (uuid) REFERENCES archive(uuid) ON DELETE CASCADE;
 ALTER TABLE hash_uuid ADD UNIQUE(uuid);
 ALTER TABLE hash_uuid ALTER COLUMN type SET DEFAULT 'uuid';
-CREATE INDEX idx_feed_hash_sha1 ON hash_sha1 (detecttime DESC, severity DESC, confidence DESC);
+CREATE INDEX idx_feed_hash_uuid ON hash_uuid (detecttime DESC, severity DESC, confidence DESC, restriction DESC);
+CREATE INDEX idx_query_hash_uuid ON hash_uuid (lower(hash), detecttime DESC, severity DESC, confidence DESC, restriction DESC);
