5bca7b04c1f3d6b87e7b578a6eadaf06b8a9f745
saxjazman9@gmail.com <saxjazman9@gmail.com@f9ffa795-1512-f4ca-611e-4b753ca244e6>
2011-11-16 12:35:41 +0000

    bugfix, skip arrays and hashrefs in csv output plugin

--- trunk/client/perl/CIF-Client/lib/CIF/Client/Plugin/Csv.pm
+++ trunk/client/perl/CIF-Client/lib/CIF/Client/Plugin/Csv.pm
@@ -9,12 +9,19 @@ sub write_out {
     my $config = shift;
     my $feed = shift;
     my @array = @{$feed->{'feed'}->{'entry'}};
-    my @header = keys(%{$array[0]});
+    #my @header = keys(%{$array[0]});
+    my @header;
+    # skip things like arrays and hashrefs for now
+    foreach (keys %{$array[0]}){
+        next unless(!ref($array[0]{$_}));
+        push(@header,$_);
+    }
     @header = sort { $a cmp $b } @header;
     my $body = '';
     foreach my $a (@array){
         delete($a->{'message'}); 
-        $body .= join(',',map { $a->{$_} ? $a->{$_} : ''} @header)."\n";
+        # the !ref() bits skip things like arrays and hashref's for now...
+        $body .= join(',',map { ($a->{$_} && !ref($a->{$_})) ? $a->{$_} : ''} @header)."\n";
     }
     my $text = '# '.join(',',@header);
     $text .= "\n".$body;
